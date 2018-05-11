9ebae03fb7162d76a4cc61cbb402df5a1681ba05
Zach Brun <zachbrun@Zach-Bruns-Mac-Mini.local>
2018-05-11 00:34:24 -0400

    ravenous pt 1

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -18,19 +18,19 @@ class Business extends React.Component {
     return (
       <div className="Business">
         <div className="image-container">
-          <img src={this.imageSrc} alt=''/>
+          <img src={business.imageSrc} alt=''/>
         </div>
-        <h2>{this.name}</h2>
+        <h2>{business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>{this.address}</p>
-            <p>{this.city}</p>
-            <p>{this.state} {this.zipCode}</p>
+            <p>{business.address}</p>
+            <p>{business.city}</p>
+            <p>{business.state} {business.zipCode}</p>
           </div>
           <div className="Business-reviews">
-            <h3>{this.category}</h3>
-            <h3 className="rating">{this.rating} stars</h3>
-            <p>{this.reviewCount} reviews</p>
+            <h3>{business.category}</h3>
+            <h3 className="rating">{business.rating} stars</h3>
+            <p>{business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
