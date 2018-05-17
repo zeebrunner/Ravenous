5885bcf11e909a9368666f74640e66dc6c9c235b
zeebrunner <zach.brun14@gmail.com>
2018-05-17 13:19:36 -0400

    added yelp.js

---
+++ src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'QRj3RDobjlfr7R01KAZZoGwkk6YvN8Kom1sUTyKzlJU6tqYcmA28kjedmi0Z6-nLxDbHpPmxvHSD_NpXqh6SOreGrNuWTCwo7DnJpspeZj0ZCWCeZwFaaGmHU3_9WnYx'
+
+const Yelp = {
+  search(term, location, sortBy){
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        { Authorization: `Bearer ${apiKey}`, }
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if(jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
